490ea869035be4c78f5241265091fd2115d1348f
Mattias Singmyr <mattias@singmyr.io>
2019-07-16 18:38:24 +0200

    Adds drone config. Adds line that fails lint.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { default as logo2 } from './logo.svg';
 
 const App: React.FC = (): JSX.Element => {
     return (
